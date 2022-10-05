903bc2f85d9600f91992d27d88ff7b2ae0a2207c
naumanakram-km <nauman.akram@kryptomind.com>
2022-10-05 17:20:47 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="./logo.svg" className="App-logo" alt="logo" />
+        <img src={"./logo.svg"} className="App-logo" alt="logo" />
         <p>
           hello from windows server
         </p>
