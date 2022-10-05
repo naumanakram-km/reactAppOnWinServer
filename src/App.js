e62dd0343b1e7ff01f15c228e4283ab7c95933d5
naumanakram-km <nauman.akram@kryptomind.com>
2022-10-05 17:17:21 +0500

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <>
+        <img src="./logo.svg" className="App-logo" alt="logo" />
+        <p>
           hello from windows server
         </p>
         <a
